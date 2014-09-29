53b39dc7b3f7eef1ef46e2cf2017f732450996e4
Stephen Fox <stevemasta34@gmail.com>
2014-09-29 08:58:15 -0600

    Built the foundation for the Presenter class that will be the UI

---
+++ src/csc130nim/Presenter.java
@@ -0,0 +1,36 @@
+package csc130nim;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Stephen on 9/29/2014.
+ * In project: csc130_nim
+ */
+public class Presenter {
+
+    private static final BufferedReader reader =
+            new BufferedReader(new InputStreamReader(System.in));
+
+    /**
+     *
+     * @param append the type of value being asked for.
+     * @return the integer input from the user.
+     */
+    public int promptNumPieces(String append) {
+
+        System.out.println("Please input " + append);
+
+        int i = 0;
+
+        try {
+            i = Integer.parseInt(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return i == 0 ? promptNumPieces(append + " properly") : i;
+    }
+
+}
