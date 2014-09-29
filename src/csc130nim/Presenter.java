4fab4bdc23e030a427d52c0f9121ee1aaaab9efb
Stephen Fox <stevemasta34@gmail.com>
2014-09-29 12:12:22 -0600

    The aforementioned debatable implementation of the MenuOptions enum is commented out.

--- src/csc130nim/Presenter.java
+++ src/csc130nim/Presenter.java
@@ -3,7 +3,6 @@ package csc130nim;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 
 /**
  * Created by Stephen on 9/29/2014.
@@ -45,11 +44,10 @@ public class Presenter {
     }
 
     public void printOpeningMenu() {
-        new PrintWriter(System.out)
-                .printf("1. Player vs. Player")
-                .printf("2. Player vs. AI")
-                .printf("3. AI vs. AI")
-                .println("0. Exit");
+        MenuOptions[] enumValues = MenuOptions.values();
+        for (int i = 0; i < enumValues.length; i++) {
+            System.out.println((i+1) + ". " + enumValues[i]);
+        }
     }
 
     enum MenuOptions {
@@ -58,6 +56,6 @@ public class Presenter {
         AIVsAI,
         Exit;
 
-        public abstract void execute();
+//        public abstract void execute();
     }
 }
