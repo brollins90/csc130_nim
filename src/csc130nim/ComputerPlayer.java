77c909ae14bac028bc8ca2b5cca3b763742c07b6
Blake Rollins <brollins90@gmail.com>
2014-09-29 19:34:35 -0600

    added the computer player and human player classes again

---
+++ src/csc130nim/ComputerPlayer.java
@@ -0,0 +1,17 @@
+package csc130nim;
+
+public class ComputerPlayer extends Player {
+
+	@Override
+	public int getRow() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberToRemove() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
