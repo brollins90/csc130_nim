f0dd09fa4d9dc4e08fa073d0eca3f5f4e58d95d0
Blake Rollins <brollins90@gmail.com>
2014-09-29 21:57:35 -0600

    Stephen wrote this code and everything is getting more abstracted. There is an enum MenuOption to implement the command pattern, which may be whiched to an interfaced "ICommand"-typed approach.

--- src/csc130nim/MenuOption.java
+++ src/csc130nim/MenuOption.java
@@ -5,10 +5,34 @@ package csc130nim;
  * In project: csc130_nim
  */
 public enum MenuOption {
-    PvP("Player vs. Player", 1),
-    PvE("Player vs. AI", 2), // Player versus Engine
-    EvE("AI vs. AI", 3),
-    Exit("Exit", 0);
+    PvP("Player vs. Player", 1) {
+		@Override
+		public void execute(Manager manager) {
+			System.out.println("Playing a Two-Player game...");
+			manager.StartGame(new HumanPlayer(), new HumanPlayer());
+		}
+	},
+    PvE("Player vs. AI", 2) {
+		@Override
+		public void execute(Manager manager) {
+			System.out.println("Playing Player vs. AI game...");
+			manager.StartGame(new HumanPlayer(), new ComputerPlayer());
+		}
+	}, // Player versus Engine
+    EvE("AI vs. AI", 3) {
+		@Override
+		public void execute(Manager manager) {
+			System.out.println("Playing AI vs AI game...");
+			manager.StartGame(new ComputerPlayer(), new ComputerPlayer());
+		}
+	},
+    Exit("Exit", 0) {
+		@Override
+		public void execute(Manager manager) {
+			System.out.println("Thanks for playing");
+			System.exit(0);
+		}
+	};
 
     private final String readableName;
     private final int retValue;
@@ -25,6 +49,8 @@ public enum MenuOption {
     public int getRetValue() {
         return retValue;
     }
+
+	public abstract void execute(Manager manager);
 
     //        public abstract void execute();
 }
